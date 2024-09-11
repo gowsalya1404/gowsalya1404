ce51f338182c07ea74eb506094aba9af11314277
T.Gowsalya <152077199+gowsalya1404@users.noreply.github.com>
2024-09-11 14:31:48 +0530

    Create Loop.java

---
+++ Loop.java
@@ -0,0 +1,23 @@
+import java.io.*;
+ 
+class GFG {
+    public static void main (String[] args) {
+      int i=0;
+      do
+      {
+        System.out.println(i);
+        i++;
+      }while(i<=10);
+    }
+}import java.io.*;
+ 
+class GFG {
+    public static void main (String[] args) {
+      int i=0;
+      do
+      {
+        System.out.println(i);
+        i++;
+      }while(i<=10);
+    }
+}
